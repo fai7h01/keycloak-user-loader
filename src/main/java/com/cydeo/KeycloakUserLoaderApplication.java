980692a490c910a8854964341049eac814063907
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-08 19:23:05 +0400

    Initial commit

---
+++ src/main/java/com/cydeo/KeycloakUserLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakUserLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakUserLoaderApplication.class, args);
+    }
+
+}
